00407512133a6626669ea4f83a969ecec52a4a8b
jellanarta <jellan.arta12@gmail.com>
2023-11-15 17:32:06 +0700

    oke

--- src/app/components/pengalamankerja.tsx
+++ src/app/components/pengalamankerja.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 export default function Pengalamankerja() {
   return (
     <div className="max-w-5xl mx-auto p-5 my-5">
-      <div className="text-center">
-        <div className="text-base uppercase font-semibold">
+      <div className="">
+        <div className="text-lg uppercase font-semibold">
           pengalaman kerja
         </div>
         <div className="text-sm">
@@ -12,38 +12,35 @@ export default function Pengalamankerja() {
         </div>
       </div>
       <div className="grid grid-cols-1 gap-5 sm:flex sm:flex-wrap sm:justify-center mt-7 lg:grid lg:grid-cols-3">
-        <Kotakkotak ikon="facebook" judul="pembuat website full stack" deskripsi="Membangun web responsif menggunakan teknologi Full stack seperti NodeJS, NextJS, ExpressJS, TailwindCSS dan MysQL" />
-        <Kotakkotak ikon="facebook" judul="spesialis seo dan konten website" deskripsi="Mengoptimalkan seitus web untuk meningkatkan peringkat pencarian melalui strategi SEO yang terukur" />
-        <Kotakkotak ikon="facebook" judul="penulis konten independen" deskripsi="Menulis artikel berkualitas tinggi tentang berbagai topik, termasuk teknologi, web development, dan tren industri terkini" />
+        <Kotakkotak bg="web" ikon="website" judul="pembuat website full stack" deskripsi="Membangun web responsif menggunakan teknologi Full stack seperti NodeJS, NextJS, ExpressJS, TailwindCSS dan MysQL" />
+        <Kotakkotak bg="seo" ikon="seo" judul="spesialis seo dan konten website" deskripsi="Mengoptimalkan seitus web untuk meningkatkan peringkat pencarian melalui strategi SEO yang terukur" />
+        <Kotakkotak bg="writing" ikon="writing" judul="penulis konten independen" deskripsi="Menulis artikel berkualitas tinggi tentang berbagai topik, termasuk teknologi, web development, dan tren industri terkini" />
       </div>
     </div>
   )
 }
 
-function Kotakkotak({ ikon, judul, deskripsi }: { ikon: string, judul: string, deskripsi: string }) {
+function Kotakkotak({ ikon, judul, deskripsi, bg }: { bg: string, ikon: string, judul: string, deskripsi: string }) {
   return (
-    <div className="ring-1 ring-gray-200 p-5 rounded-sm bg-white dark:bg-gray-700 dark:ring-gray-600 shadow-sm w-full sm:w-[calc(50%-60px)] lg:w-full">
-      <div className="grid grid-cols-[auto,1fr] gap-3">
-        <div className="w-10 h-10">
-          <Image
-            src={`/sosmed/${ikon}.svg`}
-            width={100}
-            height={100}
-            alt="ikon"
-          />
-        </div>
-        <div className="text-sm uppercase font-semibold">
-          {judul}
-        </div>
-      </div>
-      <div className="text-sm mt-5">
-        {deskripsi}
-      </div>
-      <div className="mt-5 pt-5 border-t border-gray-200 border-dotted dark:border-gray-500">
-        <div className="flex">
-          <div className="bg-blue-600 text-gray-50 p-3 text-xs uppercase">
-            Lihat detail
+    <div className={`ring-1 ring-gray-200 h-[250px] rounded-sm bg-[url('/${bg}.jpg')] bg-no-repeat bg-center bg-cover dark:ring-gray-700 shadow-sm w-full sm:w-[calc(50%-10px)] lg:w-full relative`}>
+      <div className="absolute w-full h-full p-5" style={{ background: 'rgba(0,0,0,0.8)' }}>
+        <div className="grid grid-cols-[1fr] gap-3 items-center">
+          <div className="w-14 h-14 bg-white rounded-full flex items-center justify-center">
+            <div className="w-8 h-8">
+              <Image
+                src={`/sosmed/${ikon}.svg`}
+                width={100}
+                height={100}
+                alt="ikon"
+              />
+            </div>
           </div>
+          <div className="text-lg uppercase font-semibold text-gray-50">
+            {judul}
+          </div>
+        </div>
+        <div className="text-sm mt-4 text-gray-100 dark:text-gray-200">
+          {deskripsi}
         </div>
       </div>
     </div>
