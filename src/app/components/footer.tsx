8703712956ed90669005284fc3afbb2989970291
Arta <66835016+jellanarta@users.noreply.github.com>
2023-11-19 22:57:16 +0800

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="max-w-5xl mx-auto p-5">
             <div className="text-center text-sm">
-                @copytight - Jellan Arta - 2023
+                @copyright - Jellan Arta - 2023
             </div>
         </div>
     )
